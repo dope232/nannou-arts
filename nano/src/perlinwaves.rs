df8373017831aa4572104073d411c00e493cfc8e
Dhanush S <clgdhanush232@gmail.com>
2024-07-20 23:49:44 +0530

    commit3
    commit3

---
+++ nano/src/perlinwaves.rs
@@ -0,0 +1,177 @@
+use nannou::{noise::{self, Perlin, Seedable, NoiseFn}, prelude::*};
+
+
+fn main() {
+    nannou::app(model)
+        .update(update)
+        .run();
+}
+
+
+struct Agent {
+    vector: Vec2,
+    vector_old: Vec2,
+    step_size: f32,
+    angle: f32,
+    is_outside: bool,
+    win_rect: Rect,
+}
+impl Agent {
+
+
+
+    fn new(win_rect: Rect) ->Self {
+
+        let vector = vec2(
+            random_range(win_rect.left(), win_rect.right()),
+            random_range(win_rect.top(), win_rect.bottom()),
+
+
+
+        ); 
+        Agent {
+            vector,
+            vector_old: vector,
+            step_size: random_range(1.0, 5.0),
+            angle: 0.0,
+            is_outside: false,
+            win_rect,}
+
+    
+}
+
+    fn update_default(&mut self,){
+
+        self.is_outside = false;
+        self.vector_old = self.vector;
+        self.vector.x += self.angle.cos() * self.step_size;
+        self.vector.y += self.angle.sin() * self.step_size;
+        self.is_outside = self.vector.x < self.win_rect.left() || self.vector.x > self.win_rect.right() || self.vector.y < self.win_rect.bottom() || self.vector.y > self.win_rect.top();
+        if self.is_outside {
+            self.vector = vec2(
+                random_range(self.win_rect.left(), self.win_rect.right()),
+                random_range(self.win_rect.top(), self.win_rect.bottom()),   
+            ); 
+            self.vector_old = self.vector;    
+        }
+    }
+
+    fn display(&self, draw: &Draw, stroke_weight: f32, agent_alpha: f32) {
+        draw.line()
+            .start(self.vector_old)
+            .end(self.vector)
+            .rgba(1.0, 1.0, 1.0, agent_alpha)
+            .stroke_weight(stroke_weight);
+    }
+
+    fn update(&mut self, noise: Perlin, noise_scale: f64, noise_strength: f64) {
+        let n = noise.get([
+            self.vector.x as f64 / noise_scale,
+            self.vector.y as f64 / noise_scale,
+        ]) * noise_strength;
+        self.angle = n as f32;
+    }
+
+
+    }
+
+
+
+
+
+struct Model {
+
+    agents : Vec<Agent>, 
+    noise_scale : f64,
+    noise_strength : f64, 
+    overlay: f32, 
+    agent_alpha: f32, 
+    stroke: f32, 
+    draw_mode: u8, 
+    noise_seed : u32, 
+
+
+
+}
+
+fn model(app: &App) -> Model {
+let _window = app.new_window().size(1024, 1024).view(view).key_released(key_released).build().unwrap();
+
+    const NO_OF_AGENTS : u32 = 4000; 
+    let mut agents = Vec::new(); 
+
+    for i in 0..NO_OF_AGENTS{
+        let Agent = Agent::new(app.window_rect()); 
+        agents.push(Agent); 
+
+
+    }
+
+
+    Model {
+        agents, 
+        noise_scale: 800.0,
+        noise_strength: 50.0,
+        overlay: 0.07,
+        agent_alpha: 0.75,
+        stroke: 0.2,
+        draw_mode: 1,
+        noise_seed: 3,
+
+
+    }
+}
+
+
+
+
+
+fn update(app: &App, model: &mut Model, update: Update) {
+    // Update logic here
+
+
+    let noise = Perlin::new().set_seed(model.noise_seed); 
+
+    for agent in &mut model.agents{
+        match model.draw_mode {
+            1 => agent.update(noise, model.noise_scale, model.noise_strength),
+            
+            
+            _ => (),
+        }
+        agent.update_default();
+
+    }
+
+}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    // View logic here
+let draw = app.draw();
+
+if frame.nth() == 0  {
+    draw.background().color(BLACK);
+} 
+else {
+   draw.rect().wh(app.window_rect().wh()).rgba(0.0, 0.0, 0.0, model.overlay);
+}
+
+model.agents.iter().for_each(|agent| {
+    agent.display(&draw, model.stroke,  model.agent_alpha);
+});
+
+draw.to_frame(app, &frame).unwrap();
+}
+
+
+
+fn key_released(app: &App, model: &mut Model, key: Key) {
+    match key {
+        
+        Key::S => {
+            app.main_window()
+                .capture_frame(app.exe_name().unwrap() + ".png");
+        }
+        _other_key => {}
+    }
+}
---
+++ nano/src/recursivetree.rs
@@ -0,0 +1,133 @@
+use nannou::lyon::geom::arc;
+use nannou::prelude::*;
+use nannou::lyon; 
+
+
+
+fn main() {
+    nannou::app(model)
+        .update(update)
+        .run();
+}
+
+struct Model {
+
+    recursion_level: u8, 
+    start_radius: f32, 
+
+
+}
+
+fn model(app: &App) -> Model {
+let _window = app.new_window()
+.size(1024, 1024).
+view(view)
+.key_released(key_released)
+.build()
+.unwrap();
+
+
+    Model {
+        recursion_level: 6,
+        start_radius: 200.0,
+
+    }
+}
+
+fn update(app: &App, model: &mut Model, update: Update) {
+    // Update logic here
+}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    // View logic here
+let draw = app.draw();
+
+draw.background().color(WHITE); 
+
+draw_branch(
+    &draw,
+    0.0,
+    0.0,
+    model.start_radius,
+    model.recursion_level,
+
+);
+
+
+draw.to_frame(app, &frame).unwrap();
+}
+
+// recurisve function 
+
+
+fn draw_branch(draw: &Draw,
+x: f32,
+y: f32, 
+radius : f32, 
+level: u8, 
+){
+
+
+
+    use nannou::geom::path::Builder; 
+    let mut builder = Builder::new().with_svg(); 
+    builder.move_to(lyon::math::point(x-radius, y)); 
+    builder.arc(
+    lyon::math::point(x, y),
+    lyon::math::vector(radius,radius),
+    lyon::math::Angle::two_pi(),
+    lyon::math::Angle::radians(90.0)
+); 
+
+
+
+       
+    let arc_path = builder.build(); 
+
+
+    // draw arc 
+
+    draw.path()
+    .stroke()
+    .stroke_weight(level as f32 * 1.0)
+    .rgba(0.0, 0.0, 0.0 , 1.0)
+    .caps_round()
+    .events(arc_path.iter()); 
+
+
+   
+
+    if level > 0 {
+        draw_branch(&draw, x-radius, y-radius, radius/2.0, level-1,); //left
+        draw_branch(&draw, x+radius, y-radius/2.0, radius/2.0, level-1,); //right 
+
+
+    }
+
+
+
+
+
+
+}
+
+
+fn key_released(app: &App, model: &mut Model, key: Key) {
+    match key {
+        Key::Key1 => model.recursion_level = 1,
+        Key::Key2 => model.recursion_level = 2,
+        Key::Key3 => model.recursion_level = 3,
+        Key::Key4 => model.recursion_level = 4,
+        Key::Key5 => model.recursion_level = 5,
+        Key::Key6 => model.recursion_level = 6,
+        Key::Key7 => model.recursion_level = 7,
+        Key::Key8 => model.recursion_level = 8,
+        Key::Key9 => model.recursion_level = 9,
+        Key::Key0 => model.recursion_level = 0,
+        Key::S => {
+            app.main_window()
+                .capture_frame(app.exe_name().unwrap() + ".png");
+        }
+        _other_key => {}
+    }
+}
